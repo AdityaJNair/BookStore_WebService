45c1d9803c5b8afff7c9b95eb4f0f03d7124c31d
AdityaJNair <adijnair@gmail.com>
2016-09-11 18:54:50 +1200

    changed test name

--- src/test/library/StoreWebServiceTest.java
+++ src/test/library/StoreWebServiceTest.java
@@ -8,7 +8,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class BookStoreWebServiceTest {
+public class StoreWebServiceTest {
 	private static final String WEB_SERVICE_URI = "http://localhost:10000";
 
 	@BeforeClass
